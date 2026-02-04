be4b212e7b5a9f96c5328de97c5b5316b3614650
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-02-04 11:39:18 +0530

    Router-dom initalized and packages has been installed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div className="text-2xl text-blue-900 font-extrabold">
-        PSG Institutions
-      </div>
+      <Routes>
+          <Route path="" element={<div>PSG Institutions</div>} />
+      </Routes>
     </>
   );
 }
