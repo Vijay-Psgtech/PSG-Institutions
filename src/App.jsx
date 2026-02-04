1d6104ed6901028177d85320ad33a7771ba594fa
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-02-04 11:27:51 +0530

    Initial commit with packages installed.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-blue-900 font-extrabold">
+        PSG Institutions
+      </div>
+    </>
+  );
+}
+
+export default App;
