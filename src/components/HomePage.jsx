8323153931d065c2bcc6ff7b5eecb37d5d97da8a
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-02-06 10:16:42 +0530

    Stats Component and data file has been added.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeroBanner from "../pages/Home/HeroBanner";
 import AboutSection from "../pages/Home/AboutSection";
 import InstitutionSection from "../pages/Home/InstitutionSection";
+import StatsSection from "../pages/Home/Stats";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <HeroBanner />
       <AboutSection />
       <InstitutionSection />
+      <StatsSection />
     </div>
   );
 };
---
+++ src/components/data/StatsData.jsx
@@ -0,0 +1,36 @@
+import { Users, Building2, Briefcase, Award } from "lucide-react";
+
+export const statsData = [
+  {
+    id: 1,
+    label: "Students Enrolled",
+    value: 25000,
+    suffix: "+",
+    icon: Users,
+    description: "Active learners pursuing excellence across all programs",
+  },
+  {
+    id: 2,
+    label: "Institutions",
+    value: 17,
+    suffix: "",
+    icon: Building2,
+    description: "World-class educational facilities and infrastructure",
+  },
+  {
+    id: 3,
+    label: "Faculty & Staff",
+    value: 5000,
+    suffix: "+",
+    icon: Briefcase,
+    description: "Experienced educators and support professionals",
+  },
+  {
+    id: 4,
+    label: "Industry Partners",
+    value: 1000,
+    suffix: "+",
+    icon: Award,
+    description: "Leading companies for placements and collaborations",
+  },
+];
