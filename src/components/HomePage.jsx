9092377efcb2c3af781290e978ad10762b698395
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-02-06 16:38:35 +0530

    Alumnitestimonials cards and bg gradient has been changed.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,8 +12,8 @@ const HomePage = () => {
   return (
     <div>
       <HeroBanner />
-      <InstitutionSection />
       <AboutSection />
+      <InstitutionSection />
       <StatsSection />
       <EventsSection />
       <TrusteeSection />
