4b16342d531afd3119562a4ed0c1bb1b867103db
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-02-09 14:29:02 +0530

    BacktoTop button has been added.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Routes, Route } from "react-router-dom";
 /* Layout */
 import Layout from "./components/Layout";
 
+import BacktoTop from "./components/BacktoTop";
 import HomePage from "./components/HomePage";
 import Institutions from "./pages/Institutions/Institutions";
 
 function App() {
   return (
     <>
+      <BacktoTop />
       <Routes>
         <Route path="" element={<Layout />}>
           <Route index element={<HomePage />} />
