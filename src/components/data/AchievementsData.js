e28249b0eb07817e7a10ab6d2bcdc87936a433f9
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2026-03-24 11:09:15 +0530

    Achievements json data file has been added.

---
+++ src/components/data/AchievementsData.js
@@ -0,0 +1,45 @@
+import psgcaslogo from "/images/logos/psgcas-logo.png";
+import psgctlogo from "/images/logos/psgct-logo.png";
+import psgimlogo from "/images/logos/psgim-logo.png";
+import psgiaslogo from "/images/logos/psgias-logo.png";
+import psgitechlogo from "/images/logos/psgitech-logo.png";
+import psgiaplogo from "/images/logos/psgiap-logo.webp";
+import psgpolylogo from "/images/logos/psgpoly-logo.png";
+
+import psgimsrlogo from "/images/logos/psgimsr-logo.png";
+import psgnursinglogo from "/images/logos/psgnursing-logo.png";
+import psgphysiologo from "/images/logos/psgphysio-logo.png";
+import psgpharmalogo from "/images/logos/psgpharma-logo.png";
+
+export const Achievements = [
+  {
+    title: "National Institutional Ranking Framework (NIRF)",
+    desc: "PSG College of Arts and Science has been ranked #10 among colleges in India in NIRF 2025. This achievement stands as a testimony to the high standards of academic excellence upheld by the college for over seven decades, and the unwavering commitment and dedication demonstrated by its faculty and staff.",
+    logo: psgcaslogo,
+  },
+  {
+    title: "National Institutional Ranking Framework (NIRF)",
+    desc: "PSG College of Technology, Coimbatore has been ranked #67 in Engineering Category",
+    logo: psgctlogo,
+  },
+  {
+    title: "Elite Education &Institutional Excellence Awards",
+    desc: "The leading & Trusted Management & Business College of the Year 2024 in Tamil Nadu, Presented by India Elite Education &Institutional Excellence Awards & Conference 2024",
+    logo: psgimlogo,
+  },
+  {
+    title: "National Assessment and Accreditation Council",
+    desc: "PSG iTech has been awarded the rating of A+ Grade by NAAC in 1st Cycle",
+    logo: psgitechlogo,
+  },
+  {
+    title: "COA National Awards 2025",
+    desc: "COA National Awards 2025",
+    logo: psgiaplogo,
+  },
+  {
+    title: "National Institutional Ranking",
+    desc: "41st Rank India Rankings 2024 in medical; Overall 151-200",
+    logo: psgimsrlogo,
+  },
+];
